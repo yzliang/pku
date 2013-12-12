9e5988c03851e39d9cf63f70a8caf0f516abf8dc
Yizhong Liang <liang.yizhong@epfl.ch>
2013-12-12 13:13:26 +0100

    accepted

--- 1012.cc
+++ 1012.cc
@@ -3,9 +3,15 @@ using namespace std;
 
 int main() {
   int n;
+  int cache[15] = {0};
   while (cin >> n) {
     if (n == 0) break;
-    for (int m = n + 1; ; m++) {
+    if (cache[n] != 0) {
+      cout << cache[n] << endl;
+      continue;
+    }
+    int m = n + 1;
+    while (true) {
       int i = n * 2, j = (m - 1) % i;
       while (i > n && j >= n) {
         i--;
@@ -13,8 +19,13 @@ int main() {
       }
       if (i == n) {
         cout << m << endl;
+        cache[n] = m;
         break;
       }
+      if (m % n == 0)
+        m += n + 1;
+      else
+        m++;
     }
   }
   return 0;

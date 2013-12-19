aa372ce364703530588d005ed234a5796aa47de1
Yizhong Liang <liang.yizhong@epfl.ch>
2013-12-19 11:05:16 +0100

    1033

--- 1033.cc
+++ 1033.cc
@@ -7,7 +7,7 @@ int k, n;
 int s[10000], base[10001], file_id[10000];
 bool filled[10000];
 vector<vector<int> > pos;  // position of file i's j-th block.
-bool perfect;
+bool perfect = true;
 
 void fill_empty() {
   queue<int> holes;  // empty holes.
@@ -33,30 +33,31 @@ void fill_empty() {
 
 void swap_wrong() {
   int free = base[k];
-  int wrong_id, wrong_offset, hole = -1, target;
+  queue<int> holes;
   for (int i = 0; i < k; i++) {
-    for (int j = 0; j < s[j]; j++) {
-      if (pos[i][j] != base[i] + j) {
-        wrong_id = i;
-        wrong_offset = j;
-        hole = pos[i][j];
-        target = base[i] + j;
-        break;
-      }
+    for (int j = 0; j < s[i]; j++) {
+      if (pos[i][j] != base[i] + j)
+        holes.push(base[i] + j);
     }
-    if (hole != -1) break;
   }
-  if (hole == -1) return;
-  perfect = false;
-  cout << hole + 1 << ' ' << free + 1 << endl;
-  // Chain reaction.
-  while (hole != target) {
-    int id = file_id[hole];
-    int offset = hole - base[id];
-    cout << pos[id][offset] + 1 << ' ' << hole + 1 << endl;
-    hole = pos[id][offset];
+  perfect = perfect && holes.empty();
+  while (!holes.empty()) {
+    int target = holes.front();
+    holes.pop();
+    int id = file_id[target];
+    int offset = target - base[id];
+    if (pos[id][offset] == target) continue;  // already fixed.
+    cout << pos[id][offset] + 1 << ' ' << free + 1 << endl;
+    int hole = pos[id][offset];
+    while (hole != target) {
+      id = file_id[hole];
+      offset = hole - base[id];
+      cout << pos[id][offset] + 1 << ' ' << hole + 1 << endl;
+      hole = pos[id][offset];
+      pos[id][offset] = base[id] + offset;
+    }
+    cout << free + 1 << ' ' << target + 1 << endl;
   }
-  cout << free + 1 << ' ' << hole + 1 << endl;
 }
 
 int main() {
